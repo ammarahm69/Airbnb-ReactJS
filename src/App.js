83abe0876e119ee7975b1287e4757eea2e2333bc
Ammar Ahmed <ammar69aa59@gmail.com>
2024-07-20 00:36:52 -0700

    Routes add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import "./App.css";
 import Home from "./Home";
 import Header from "./Header";
+import Footer from "./Footer";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SearchPage from './Searchpage'
+
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
+     // BEM
+     <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/Searchpage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Searchpage.css'
+function Searchpage() {
+  return (
+    <div className='searchpage'>
+      <h1>I,m Search Page</h1>
+    </div>
+  )
+}
+
+export default Searchpage
